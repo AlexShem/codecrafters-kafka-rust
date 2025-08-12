d4a2c7d84539f30f7db2c3cf362a1161bd73c25b
AlexShem <alex.shemendyuk@gmail.com>
2025-08-12 21:36:56 +0200

    Stage 2 #NV3
    - Send Correlation ID

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+
+use std::io::{BufWriter, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
@@ -7,11 +9,23 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                handle_connection(_stream).expect("Failed to handle connection");
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(stream: TcpStream) -> anyhow::Result<()> {
+    let mut writer = BufWriter::new(stream);
+
+    let message_size: i32 = 0;
+    let correlation_id: i32 = 7;
+
+    writer.write_all(message_size.to_be_bytes().as_slice())?;
+    writer.write_all(correlation_id.to_be_bytes().as_slice())?;
+
+    Ok(())
 }
